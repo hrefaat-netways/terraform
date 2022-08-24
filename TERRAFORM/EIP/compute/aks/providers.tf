3bce35166bb78dfea867ed39decbaf4265377f33
hrefaat-netways <109003592+hrefaat-netways@users.noreply.github.com>
2022-08-24 19:52:42 +0200

    Update providers.tf

--- TERRAFORM/EIP/compute/aks/providers.tf
+++ TERRAFORM/EIP/compute/aks/providers.tf
@@ -1,27 +1,22 @@
+# We strongly recommend using the required_providers block to set the
+# Azure Provider source and version being used
 terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "~> 2.0"
     }
-    random = {
-      source  = "hashicorp/random"
-      version = "~> 3.0"
-
-    }
   }
-
 }
-provider "azurerm" {
-  features {
 
-  }
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+
+  subscription_id = "e9c6df82-43a6-4361-8b5c-cd513685057e"
+  client_id       = "b38f6df6-9da9-4c8c-92a1-65bddac48414"
+  client_secret   = "oxo8Q~7QHDpkrjYnbKQmzXf8FB4fZQIzUGp4Zbhx"
+  tenant_id       = "2ab2b5d0-18af-4aa0-9010-cb69d0c7b92f"
 }
 resource "random_pet" "aksrandom" {
 }
-
-
-
-
-
-

3c608eb59cdb11b8a0d665e94ddaa1fee299a65d
hrefaat-netways <109003592+hrefaat-netways@users.noreply.github.com>
2022-09-09 11:36:18 +0200

    Update backend.tf

--- TERRAFORM/EIP/compute/aks/backend.tf
+++ TERRAFORM/EIP/compute/aks/backend.tf
@@ -2,7 +2,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "Azure-Internal-RD-6202"
-    storage_account_name = "tfstorage16072022"
+    storage_account_name = "teraformadx"
     container_name       = "tfsate"
     key                  = "aks.terraform.tfstate"
   }
